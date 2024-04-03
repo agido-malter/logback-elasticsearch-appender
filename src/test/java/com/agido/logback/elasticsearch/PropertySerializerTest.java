eda4d5ae7e0f079904199af08701de1067684348
Inigo Surguy <inigo.surguy@67bricks.com>
2024-04-03 14:58:54 +0100

    Update version of Mockito to make tests compatible with JDK 17+. Updated to latest 4.x, because 5.x introduces changes that alter which method calls are detected.

--- src/test/java/com/agido/logback/elasticsearch/PropertySerializerTest.java
+++ src/test/java/com/agido/logback/elasticsearch/PropertySerializerTest.java
@@ -8,10 +8,10 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
